08ae88d21799a07b4d66cd9e10f512865f31878e
Dafrok <o.o@mug.dog>
2017-05-15 21:23:19 +0800

    Init actions

--- src/actions/init.js
+++ src/actions/init.js
@@ -4,6 +4,7 @@ import {Layout} from '@/base/factory.js'
 
 store.addAction('initLayout', payload => builder().set('pc.data.layout', new Layout()))
 
+store.addAction('initInfo', payload => builder().set('pc.data.info', {
   title: 'Sanrise Project',
   idOrder: 0
 }))
@@ -22,3 +23,16 @@ store.addAction('init', (payload, {dispatch}) => {
 store.addAction('genId', (payload, {getState}) => builder().set('pc.data.info.idOrder', getState('pc.data.info.idOrder') + 1))
 
 store.addAction('activeLayout', (payload, {getState}) => builder().set('pc.activeLayout', getState('pc.activeLayout') === payload ? null : payload))
+
+store.addAction('addLayout', (payload, {getState}) => {
+  const activeLayout = getState('pc.activeLayout')
+  return activeLayout && builder().push(`pc.data.${activeLayout}.children`, new Layout())
+})
+
+store.addAction('removeLayout', (payload, {getState, dispatch}) => {
+  const activeLayout = (getState('pc.activeLayout') || '').split('.')
+  const children = activeLayout.splice(-1)
+  const index = children[0].match(/children\[(.+)\]/)[1]
+  dispatch('activeLayout', null)
+  return activeLayout.length > 0 && builder().splice(`pc.data.${activeLayout.join('.')}.children`, index, 1)
+})
