9069376f038cf89b6b2d2c609bad2cd11f71f57a
Dafrok <o.o@mug.dog>
2017-06-04 15:04:01 +0800

    Update quick set panel

---
+++ src/config/quick-set.js
@@ -0,0 +1,20 @@
+export default {
+  resizers: ['lt', 't', 'rt', 'l', 'r', 'lb', 'b', 'rb'],
+  axis: [{
+      value: 'lt',
+      name: '左上对齐'
+    },
+    {
+      value: 'rt',
+      name: '右上对齐'
+    },
+    {
+      value: 'lb',
+      name: '左下对齐'
+    },
+    {
+      value: 'rb',
+      name: '右下对齐'
+    }
+  ]
+}
