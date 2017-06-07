fc3ac80297f841897a98a944368cfc96a757a1a3
Dafrok <o.o@mug.dog>
2017-06-07 14:06:21 +0800

    Optimize sizing arithmetic

--- src/base/factory.js
+++ src/base/factory.js
@@ -13,6 +13,11 @@ export class Layout {
     this.type = 0
     this.id = options.id || store.dispatch('genId') || `sr-${store.getState('pc.data.info.idOrder')}`
     this.children = options.children || []
-    this.style = options.style || {height: '100px', position: 'relative'}
+    this.unit = {
+      pos: 'px',
+      size: 'px'
+    }
+    this.axis = 'lt'
+    this.style = options.style || {height: '100px', position: 'absolute', width: '100px', background: 'red'}
   }
 }
